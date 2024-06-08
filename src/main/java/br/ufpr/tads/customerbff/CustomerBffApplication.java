260f2f19604bba50230c051628f7dc41c7ac8302
Juan Fernandes <juanfernandesrrm@gmail.com>
2024-06-08 17:50:35 -0300

    Initial commit

---
+++ src/main/java/br/ufpr/tads/customerbff/CustomerBffApplication.java
@@ -0,0 +1,13 @@
+package br.ufpr.tads.customerbff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerBffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ufpr/tads/customerbff/CustomerBffApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufpr.tads.customerbff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerBffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
